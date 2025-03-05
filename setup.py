2582d045deaa3c30efffd151f9fbf963e02caf0d
Anton Perepelenko <anton.perepelenko@cgi.com>
2025-03-05 16:16:27 +0200

    test: implement simple test for verification purposes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name="asar-xarray",
+    name="asar_xarray",
     version="0.0.1",
     python_requires=">=3.6",
-    packages=['asar-xarray'],
+    packages=['asar_xarray'],
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'tox>=4.23.2', 'coverage>=7.6.9'],
