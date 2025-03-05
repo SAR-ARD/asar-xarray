e8ad4a058f025ecd79ae905962766d3be2758402
Anton Perepelenko <anton.perepelenko@cgi.com>
2025-03-05 16:12:19 +0200

    ci: add coverage report support

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setuptools.setup(
     version="0.0.1",
     python_requires=">=3.6",
     packages=['asar-xarray'],
-    install_requires=[]
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'tox>=4.23.2', 'coverage>=7.6.9'],
+    test_suite='tests',
 )
