f3ddfe6a835bf0265d3cb411dcccdb97818a858c
Anton Perepelenko <anton.perepelenko@cgi.com>
2025-03-12 12:15:26 +0200

    tests: use conda for dependency management

--- asar_xarray/reader.py
+++ asar_xarray/reader.py
@@ -10,6 +10,11 @@ def get_gdal_dataset(filepath: str) -> gdal.Dataset:
     :param filepath: File to be read
     :return: Opened dataset
     """
+    print("Available GDAL drivers:")
+    for i in range(gdal.GetDriverCount()):
+        driver = gdal.GetDriver(i)
+        print(driver.ShortName)
+
     dataset: gdal.Dataset = gdal.Open(filepath)
     if not dataset:
         raise RuntimeError(f'Could not open file: {filepath}')
