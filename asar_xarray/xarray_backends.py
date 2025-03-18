1d49d63af95c2801585b0e0bf728cf0230fa5cb8
Anton Perepelenko <anton.perepelenko@cgi.com>
2025-03-18 14:09:23 +0200

    ci: Configure type checking (#33)

--- asar_xarray/xarray_backends.py
+++ asar_xarray/xarray_backends.py
@@ -1,4 +1,10 @@
+import os
+from typing import Any, Iterable
+
 import xarray as xr
+from xarray.backends import AbstractDataStore
+from xarray.core.types import ReadBuffer
+
 from asar_xarray import asar
 
 
@@ -7,6 +13,9 @@ class AsarBackend(xr.backends.common.BackendEntrypoint):
     Xarray backend of ESR/ASAR satellite data.
     """
 
-    def open_dataset(self, filename_or_obj, *, drop_variables) -> xr.Dataset:
+    def open_dataset(self,
+                     filename_or_obj: str | os.PathLike[Any] | ReadBuffer[Any] | AbstractDataStore,
+                     *,
+                     drop_variables: str | Iterable[str] | None = None, ) -> xr.Dataset:
         dataset = asar.open_asar_dataset(filename_or_obj)
         return dataset
