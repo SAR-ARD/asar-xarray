292ecdd7d2f1a881505d8a771153ddd27828a87b
Achaad <achaad@achaad.eu>
2025-06-13 11:35:45 +0300

    tests: fix test_xarray_backends.py

--- tests/test_xaray_backends.py
+++ tests/test_xaray_backends.py
@@ -9,6 +9,6 @@ def test_open_dataset(resources_dir: str) -> None:
     file = os.path.join(resources_dir, 'ASA_IMS_1PNESA20040109_194924_000000182023_00157_09730_0000.N1')
     dataset = xr.open_dataset(file, engine='asar')
     assert dataset is not None
-    assert dataset.dims['x'] == 5174
-    assert dataset.dims['y'] == 30181
-    assert dataset.data_vars['pixel_values'].data.shape == (30181, 5174)
+    assert dataset.dims['slant_range_time'] == 5174
+    assert dataset.dims['azimuth_time'] == 30181
+    assert dataset.data_vars['measurements'].data.shape == (30181, 5174)
